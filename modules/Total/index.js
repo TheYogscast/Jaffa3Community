82be69e72419bd3162e6060e5d9c72c28faa8a6d
MattIPv4 <me@mattcowley.co.uk>
2019-12-08 12:52:41 +0000

    Wording tweaks

--- modules/Total/index.js
+++ modules/Total/index.js
@@ -7,10 +7,10 @@ const getBold = (string, discord) => {
 };
 
 module.exports = {
-  name: 'Total',
+  name: 'Jingle Jam Total',
   module(jaffamod) {
     jaffamod.registerCommand('total', (message, reply, discord) => {
-      // Determine if it's JingleJam time
+      // Determine if it's Jingle Jam time
       //  (December + first 7 days of the new year to view the total raised)
       const d = new Date();
       if (d.getMonth() === 11 || (d.getMonth() === 0 && d.getDate() <= 7)) {
@@ -18,10 +18,10 @@ module.exports = {
           reply(`The total amount couldn't be determined currently. ${getEmote('yogP3', discord)} Please try again later.`);
         }).then(data => {
           const year = d.getMonth() === 11 ? d.getFullYear() : d.getFullYear() - 1; // Account for being in January
-          reply(`We've currently raised a total of ${getBold(`$${data.formatted_total}`, discord)} for charity during JingleJam ${year} so far! Donate now at https://humble.com/yogs`);
+          reply(`We've currently raised a total of ${getBold(`$${data.formatted_total}`, discord)} for charity during Jingle Jam ${year} so far! Donate now at https://humble.com/yogs`);
         });
       } else {
-        reply(`It's currently not JingleJam time. ${getEmote('yogP3', discord)} We look forward to seeing you on Twitch in December to raise more money for charity once again!`);
+        reply(`It's currently not Jingle Jam time. ${getEmote('yogP3', discord)} We look forward to seeing you in December to raise more money for charity once again!`);
       }
     });
   }
