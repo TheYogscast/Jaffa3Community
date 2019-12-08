6bd5162b6e3a3f5ad8d42c2c9c182e39e24630ef
MattIPv4 <me@mattcowley.co.uk>
2019-12-08 15:34:09 +0000

    Fixes

--- modules/Total/index.js
+++ modules/Total/index.js
@@ -7,18 +7,18 @@ const getBold = (string, discord) => {
 };
 
 module.exports = {
-  name: 'Jingle Jam Total',
+  name: 'Total',
   module(jaffamod) {
     jaffamod.registerCommand('total', (message, reply, discord) => {
       // Determine if it's Jingle Jam time
       //  (December + first 7 days of the new year to view the total raised)
       const d = new Date();
       if (d.getMonth() === 11 || (d.getMonth() === 0 && d.getDate() <= 7)) {
-        jaffamod.api.get('https://jinglejam.yogscast.com/api/total').catch(() => {
-          reply(`The total amount couldn't be determined currently. ${getEmote('yogP3', discord)} Please try again later.`);
-        }).then(data => {
+        jaffamod.api.get('https://jinglejam.yogscast.com/api/total').then(data => {
           const year = d.getMonth() === 11 ? d.getFullYear() : d.getFullYear() - 1; // Account for being in January
           reply(`We've currently raised a total of ${getBold(`$${data.formatted_total}`, discord)} for charity during Jingle Jam ${year} so far! Donate now at https://humble.com/yogs`);
+        }).catch(() => {
+          reply(`The total amount couldn't be determined currently. ${getEmote('yogP3', discord)} Please try again later.`);
         });
       } else {
         reply(`It's currently not Jingle Jam time. ${getEmote('yogP3', discord)} We look forward to seeing you in December to raise more money for charity once again!`);
