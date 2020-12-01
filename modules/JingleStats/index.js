a1e426968db9a22ce12cc51d64c8fa5f4010d373
MattIPv4 <me@mattcowley.co.uk>
2020-12-01 15:21:01 +0000

    Refactor and use the 2020 API/bundle link

--- modules/JingleStats/index.js
+++ modules/JingleStats/index.js
@@ -1,4 +1,4 @@
-const formatDollars = value => `$${value.toLocaleString(undefined, { maximumFractionDigits: 2 })}`;
+const formatMoney = value => `£${value.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
 const shookEmote = (jaffamod, discord) => {
   const shook = ['yogWow', 'yogSog', 'yogPog', 'yogPag', 'yogLog', 'yogBog'];
   return jaffamod.utils.getEmote(shook[Math.floor(Math.random() * shook.length)], discord);
@@ -9,46 +9,50 @@ module.exports = {
   module(jaffamod) {
     jaffamod.registerCommand('jinglestats', (message, reply, discord) => {
       // Only run during JingleJam + first week of January
-      if (jaffamod.utils.isJingleJamExt()) {
-        jaffamod.api.get('https://www.humblebundle.com/jinglejam_stats').then(res => {
-          // Validate the response from humble
-          if (!res || !res.data || !res.data.units || !res.data.gmv) {
-            console.error(`Couldn't run jinglestats command, got bad data`, res.data);
-            throw new Error(); // Force ourselves into the catch block
-          }
+      if (!jaffamod.utils.isJingleJamExt())
+        return reply(`It's not currently Jingle Jam time. ${jaffamod.utils.getEmote('yogP3', discord)} We look forward to seeing you in December to raise more money for charity once again!`);
 
-          // Get the year, accounting for being in January
-          const d = new Date();
-          const year = d.getMonth() === 11 ? d.getFullYear() : d.getFullYear() - 1;
+      // Bundle hasn't yet launched
+      const d = new Date();
+      if (d.getMonth() === 11 && d.getDate() === 1 && d.getHours() < 17)
+        return reply(`The Jingle Jam bundle hasn't launched yet! ${jaffamod.utils.getEmote('yogP3', discord)} Get ready to get the bundle and raise some money for charity once again!`);
 
-          // Time since launch
-          const hoursSinceLaunch = (d - new Date(year, 11, 1, 17, 0, 0, 0)) / 1000 / 60 / 60;
-          const daysSinceLaunch = hoursSinceLaunch / 24;
+      // Get the total raised from the magical API
+      jaffamod.api.get('https://jinglejam.yogscast.com/api/total').then(res => {
+        // Validate the response from API
+        if (!res || !res.data || !res.data.total || !res.data.donations_with_reward) {
+          console.error(`Couldn't run jinglestats command, got bad data`, res.data);
+          throw new Error(); // Force ourselves into the catch block
+        }
 
-          // Stats!
-          const total = jaffamod.utils.getBold(formatDollars(parseFloat(res.data.gmv)), discord);
-          const bundles = jaffamod.utils.getBold(res.data.units.toLocaleString(), discord);
-          const perBundle = jaffamod.utils.getBold(formatDollars(parseFloat(res.data.gmv) / res.data.units), discord);
-          const perHour = jaffamod.utils.getBold(formatDollars(parseFloat(res.data.gmv) / hoursSinceLaunch), discord);
-          const bundlesPerHour = jaffamod.utils.getBold(Math.round(res.data.units / hoursSinceLaunch).toLocaleString(), discord);
-          const perDay = jaffamod.utils.getBold(formatDollars(parseFloat(res.data.gmv) / daysSinceLaunch), discord);
-          const bundlesPerDay = jaffamod.utils.getBold(Math.round(res.data.units / daysSinceLaunch).toLocaleString(), discord);
+        // Get the year, accounting for being in January
+        const year = d.getMonth() === 11 ? d.getFullYear() : d.getFullYear() - 1;
 
-          // Message time
-          reply(`We've raised a total of ${total} with ${bundles} bundles sold for charity during Jingle Jam ${year} so far!
- That works out to an average of ${perBundle} donated to awesome charities per bundle purchased! ${shookEmote(jaffamod, discord)}
- Per hour, that's approximately ${perHour} donated and ${bundlesPerHour} bundles sold.
- Per day, that's roughly ${bundlesPerDay} bundles sold and ${perDay} donated on average.
- Get involved by donating now at ${jaffamod.utils.getLink('https://humble.com/yogs', discord)}`);
-        })
-          .catch(() => {
-            // Web request failed or returned invalid data
-            reply(`Jingle Jam stats couldn't be determined. ${jaffamod.utils.getEmote('yogP3', discord)} Please try again later.`);
-          });
-      } else {
-        // Not currently the best time of the year :(
-        reply(`It's not currently Jingle Jam time. ${jaffamod.utils.getEmote('yogP3', discord)} We look forward to seeing you in December to raise more money for charity once again!`);
-      }
+        // Time since launch
+        // TODO: Down the road, cap this once bundle sales end, so the per hour/day stats don't get worse & worse
+        const hoursSinceLaunch = (d - new Date(year, 11, 1, 17, 0, 0, 0)) / 1000 / 60 / 60;
+        const daysSinceLaunch = hoursSinceLaunch / 24;
+
+        // Stats!
+        const total = jaffamod.utils.getBold(formatMoney(parseFloat(res.data.total)), discord);
+        const bundles = jaffamod.utils.getBold(res.data.donations_with_reward.toLocaleString(), discord);
+        const perBundle = jaffamod.utils.getBold(formatMoney(parseFloat(res.data.total) / res.data.donations_with_reward), discord);
+        const perHour = jaffamod.utils.getBold(formatMoney(parseFloat(res.data.total) / hoursSinceLaunch), discord);
+        const bundlesPerHour = jaffamod.utils.getBold(Math.round(res.data.donations_with_reward / hoursSinceLaunch).toLocaleString(), discord);
+        const perDay = jaffamod.utils.getBold(formatMoney(parseFloat(res.data.total) / daysSinceLaunch), discord);
+        const bundlesPerDay = jaffamod.utils.getBold(Math.round(res.data.donations_with_reward / daysSinceLaunch).toLocaleString(), discord);
+
+        // Message time
+        reply(`We've raised a total of ${total} with ${bundles} bundles claimed for charity during Jingle Jam ${year} so far!
+ That works out to an average of ${perBundle} donated to awesome charities per bundle claimed! ${shookEmote(jaffamod, discord)}
+ Per hour, that's approximately ${perHour} donated and ${bundlesPerHour} bundles claimed.
+ Per day, that's roughly ${bundlesPerDay} bundles claimed and ${perDay} donated on average.
+ Get involved by donating now at ${jaffamod.utils.getLink('https://jinglejam.tiltify.com', discord)}`);
+      })
+        .catch(() => {
+          // Web request failed or returned invalid data
+          reply(`Jingle Jam stats couldn't be determined. ${jaffamod.utils.getEmote('yogP3', discord)} Please try again later.`);
+        });
     });
   }
 };
